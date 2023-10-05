e46ecc47b27aaaa023abbe931bec2dd6b094aff8
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-10-05 22:15:09 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <h1>Ayushmaan</h1>
-      <p>This is ayushmaan's portfolio</p>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div id="nav-menu">
+      <ul>
+        <li>
+          <a href="#home" className="nav-link home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="nav-link about">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#skills" className="nav-link skills">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a href="#projects" className="nav-link projects">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="nav-link contact">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link resume">
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
