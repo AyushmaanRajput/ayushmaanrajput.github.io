e92dfda0c299e5ffbe698fe67d94c9096f36234f
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-10-10 15:03:22 +0530

    contact sections detailed added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div id='about' className='about section'>About</div>
+    <div id='about' className='about section'>
+      <h1>Welcome, I am <span id="user-detail-name">Ayushmaan Rajput</span>,
+      I Build things for the web.</h1>
+      <p id="user-detail-intro">I'm a web developer specializing in building ( & occasionally designing ) robust web applicatoins. I'm focused on delivering, exceptional online experiences.</p>
+    </div>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export const Contact = () => {
   return (
-    <div id='contact'>Contact</div>
-  )
-}
+    <div id="contact">
+      <h2>You can find me on,</h2>
+      <h3>
+        Github:{" "}
+        <a
+          href="https://github.com/AyushmaanRajput"
+          target="_blank"
+          id="contact-github"
+        >
+          Click here
+        </a>
+      </h3>
+      <h4>
+        LinkedIn:{" "}
+        <a
+          href="https://www.linkedin.com/in/ayushmaan-rajput-8ba7ab279/"
+          target="_blank"
+          id="contact-linkedin"
+        >
+          Click here
+        </a>
+      </h4>
+      <h4>
+        Call Me On: <span id="contact-phone">+91 7523965419</span>
+      </h4>
+      <h4>
+        Mail at: <span id="contact-email">ayushmaanrajput25feb@gmail.com</span>
+      </h4>
+    </div>
+  );
+};
